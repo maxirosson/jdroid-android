1a61089630a08892d9a643dfbf2f0223a78664a5
Maxi <maxirosson@gmail.com>
2018-04-17 22:31:12 -0300

    Added StrictModeHelper javadocs

--- jdroid-android-core/src/main/java/com/jdroid/android/strictmode/StrictModeHelper.java
+++ jdroid-android-core/src/main/java/com/jdroid/android/strictmode/StrictModeHelper.java
@@ -10,10 +10,21 @@ import com.jdroid.java.utils.LoggerUtils;
 
 import org.slf4j.Logger;
 
+/**
+ * Initialize Android Strict Mode.
+ * StrictMode is a developer tool which detects things you might be doing by accident and brings them to your attention so you can fix them.
+ *
+ * https://developer.android.com/reference/android/os/StrictMode.html
+ */
 public class StrictModeHelper {
 	
 	private final static Logger LOGGER = LoggerUtils.getLogger(StrictModeHelper.class);
 	
+	/*
+	 * Initialize Strict Mode.
+	 *
+	 * Please invoke this method on Application#onCreate(), before calling super.onCreate()
+	 */
 	public static void initStrictMode() {
 		if (isStrictModeEnabled()) {
 			LOGGER.debug("Initializing strict mode");
@@ -55,6 +66,13 @@ public class StrictModeHelper {
 		StrictMode.setVmPolicy(vmPolicyBuilder.build());
 	}
 	
+	
+	/*
+	 * Strict mode shouldn't be enabled on production, but we enable it for the release APK if it is installed on Firebase Test Lab,
+	 * so we can see the warnings detected by the tests.
+	 *
+	 * @return Whether strict mode is enabled or not.
+	 */
 	public static Boolean isStrictModeEnabled() {
 		return isStrictModeOnFirebaseTestLabEnabled() || !AppUtils.isReleaseBuildType() && BuildConfigUtils.getBuildConfigBoolean("STRICT_MODE_ENABLED", false);
 	}
