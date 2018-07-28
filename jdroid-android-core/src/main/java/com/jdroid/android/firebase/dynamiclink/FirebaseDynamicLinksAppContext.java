62991c509f57c4d47335a7941b1647e74e14f060
Maxi Rosson <maxirosson@gmail.com>
2018-07-28 00:02:23 -0300

    Add support to enable/disable Firebase Dynamic Links

--- jdroid-android-core/src/main/java/com/jdroid/android/firebase/dynamiclink/FirebaseDynamicLinksAppContext.java
+++ jdroid-android-core/src/main/java/com/jdroid/android/firebase/dynamiclink/FirebaseDynamicLinksAppContext.java
@@ -4,6 +4,10 @@ import com.jdroid.android.context.BuildConfigUtils;
 
 public class FirebaseDynamicLinksAppContext {
 
+	public static Boolean isFirebaseDynamicLinksEnabled() {
+		return BuildConfigUtils.getBuildConfigBoolean("FIREBASE_DYNAMIC_LINKS_ENABLED", true);
+	}
+	
 	public static String getDynamicLinksDomain() {
 		return BuildConfigUtils.getBuildConfigValue("FIREBASE_DYNAMIC_LINKS_DOMAIN");
 	}
