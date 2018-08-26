5168aa9a2a9a0729f72aec9f00d0abca0fe35a20
Maxi Rosson <maxirosson@gmail.com>
2018-08-25 21:12:09 -0300

    Fixed bad merge

--- jdroid-android-google-inappbilling/src/main/java/com/jdroid/android/google/inappbilling/InAppBillingActivityDelegate.java
+++ jdroid-android-google-inappbilling/src/main/java/com/jdroid/android/google/inappbilling/InAppBillingActivityDelegate.java
@@ -23,30 +23,4 @@ public class InAppBillingActivityDelegate extends ActivityDelegate {
 			inAppBillingLoaded = true;
 		}
 	}
-<<<<<<< HEAD
-=======
-
-	@Override
-	public void onResume() {
-		super.onResume();
-
-		if (InAppBillingAppModule.get().isInAppBillingEnabled()) {
-			// Important: Dynamically register for broadcast messages about updated purchases.
-			// We register the receiver here instead of as a <receiver> in the Manifest
-			// because we always call getPurchases() at startup, so therefore we can ignore
-			// any broadcasts sent while the app isn't running.
-			broadcastReceiver = new InAppBillingBroadcastReceiver();
-			getActivity().registerReceiver(broadcastReceiver, broadcastReceiver.createIntentFilter());
-		}
-	}
-
-	@Override
-	public void onPause() {
-		super.onPause();
-
-		if (broadcastReceiver != null) {
-			getActivity().unregisterReceiver(broadcastReceiver);
-		}
-	}
->>>>>>> master
 }
