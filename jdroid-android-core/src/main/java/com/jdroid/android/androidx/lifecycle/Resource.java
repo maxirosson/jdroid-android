618624b7c655226e0e87e08cef7dff8c340d7dcd
Maxi <maxirosson@gmail.com>
2018-11-10 15:40:31 -0300

    Improved Resource toString

--- jdroid-android-core/src/main/java/com/jdroid/android/androidx/lifecycle/Resource.java
+++ jdroid-android-core/src/main/java/com/jdroid/android/androidx/lifecycle/Resource.java
@@ -2,6 +2,8 @@ package com.jdroid.android.androidx.lifecycle;
 
 import com.jdroid.java.exception.AbstractException;
 
+import java.util.Collection;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
@@ -44,7 +46,12 @@ public class Resource<T> {
 	public String toString() {
 		final StringBuffer sb = new StringBuffer("Resource{");
 		sb.append("status=").append(status);
-		sb.append(", data=").append(data);
+		sb.append(", data=");
+		if (data != null & data instanceof Collection) {
+			sb.append(data.getClass().getSimpleName()  + "{length=" + ((Collection)data).size() + "}");
+		} else {
+			sb.append(data);
+		}
 		sb.append(", exception=").append(exception);
 		sb.append('}');
 		return sb.toString();
