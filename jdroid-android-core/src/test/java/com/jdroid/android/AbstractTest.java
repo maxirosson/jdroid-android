1ba9cae56a16fa36bdb2c9c04379853f0300707f
Maxi Rosson <maxirosson@gmail.com>
2019-04-24 15:26:18 -0300

    Robolectric & build.gradle upgrades

--- jdroid-android-core/src/test/java/com/jdroid/android/AbstractTest.java
+++ jdroid-android-core/src/test/java/com/jdroid/android/AbstractTest.java
@@ -10,7 +10,7 @@ import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 @RunWith(RobolectricTestRunner.class)
-@Config(manifest = Config.NONE, application = TestAndroidApplication.class, sdk = Build.VERSION_CODES.N_MR1)
+@Config(manifest = Config.NONE, application = TestAndroidApplication.class, sdk = Build.VERSION_CODES.P)
 public abstract class AbstractTest {
 
 	@Before
