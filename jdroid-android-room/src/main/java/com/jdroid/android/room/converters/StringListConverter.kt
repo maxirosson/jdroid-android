1fe63b8df8beddc575da66ecb7937242ea0eb257
Maxi Rosson <maxirosson@gmail.com>
2019-06-23 22:17:19 -0300

    Fixed room converters

--- jdroid-android-room/src/main/java/com/jdroid/android/room/converters/StringListConverter.kt
+++ jdroid-android-room/src/main/java/com/jdroid/android/room/converters/StringListConverter.kt
@@ -7,13 +7,13 @@ import com.google.gson.reflect.TypeToken
 class StringListConverter {
 
     @TypeConverter
-    fun fromString(value: String): List<String>? {
+    fun fromString(value: String?): List<String>? {
         val listType = object : TypeToken<List<String>>() { }.type
         return Gson().fromJson<List<String>>(value, listType)
     }
 
     @TypeConverter
-    fun fromList(list: List<String>): String {
+    fun fromList(list: List<String>?): String? {
         return Gson().toJson(list)
     }
 }
