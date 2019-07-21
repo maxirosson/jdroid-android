d08e8cc3133a90c4d103047b4a919eca3b219014
Maxi Rosson <maxirosson@gmail.com>
2019-07-21 17:23:32 -0300

    Fixed compilation error

--- jdroid-android-sample/src/test/java/com/jdroid/android/sample/AbstractUnitTest.kt
+++ jdroid-android-sample/src/test/java/com/jdroid/android/sample/AbstractUnitTest.kt
@@ -2,7 +2,7 @@ package com.jdroid.android.sample
 
 abstract class AbstractUnitTest : AbstractTest() {
 
-    override fun isHttpMockEnabled(): Boolean? {
+    override fun isHttpMockEnabled(): Boolean {
         return true
     }
 }
