1d3bdc03160bef91309d116855f26d1c18e085ba
Maxi Rosson <maxirosson@gmail.com>
2019-08-08 09:14:50 -0300

    More kotlin extensions

--- jdroid-android-core/src/main/java/com/jdroid/android/kotlin/FragmentExtensions.kt
+++ jdroid-android-core/src/main/java/com/jdroid/android/kotlin/FragmentExtensions.kt
@@ -1,16 +1,21 @@
 package com.jdroid.android.kotlin
 
+import android.os.Bundle
 import android.os.Parcelable
 import androidx.fragment.app.Fragment
 import java.io.Serializable
 import java.util.ArrayList
 
+val Fragment.requireArguments: Bundle
+    get() = arguments!!
+
 fun Fragment.getSerializableArgument(key: String?): Serializable? {
     return arguments?.getSerializable(key)
 }
 
-fun Fragment.getRequiredSerializableArgument(key: String?): Serializable {
-    return arguments?.getSerializable(key)!!
+@Suppress("UNCHECKED_CAST")
+fun <S : Serializable> Fragment.getRequiredSerializableArgument(key: String?): S {
+    return arguments?.getSerializable(key)!! as S
 }
 
 fun Fragment.getIntArgument(key: String?): Int? {
@@ -37,12 +42,28 @@ fun Fragment.getRequiredBooleanArgument(key: String?): Boolean {
     return arguments?.getBoolean(key)!!
 }
 
+fun Fragment.getRequiredBooleanArgument(key: String?, defaultValue: Boolean): Boolean {
+    return arguments?.getBoolean(key, defaultValue) ?: defaultValue
+}
+
 fun Fragment.getStringArgument(key: String?): String? {
     return arguments?.getString(key)
 }
 
 fun Fragment.getRequiredStringArgument(key: String?): String {
     return arguments?.getString(key)!!
+}
+
+fun Fragment.getRequiredStringArgument(key: String?, default: String): String {
+    return arguments?.getString(key, default)!!
+}
+
+fun Fragment.getLongArgument(key: String?): Long? {
+    return arguments?.getLong(key)
+}
+
+fun Fragment.getRequiredLongArgument(key: String?): Long {
+    return arguments?.getLong(key)!!
 }
 
 fun Fragment.getDoubleArgument(key: String?): Double? {
@@ -69,6 +90,11 @@ fun <T : Parcelable> Fragment.getRequiredParcelableArgument(key: String?): T {
     return arguments?.getParcelable(key)!!
 }
 
+@Suppress("UNCHECKED_CAST")
 fun <T : Any> Fragment.getRequiredArgument(key: String?): T {
     return arguments?.get(key)!! as T
+}
+
+fun Fragment.getRequiredStringArrayListArgument(key: String?): ArrayList<String> {
+    return arguments?.getStringArrayList(key)!!
 }
