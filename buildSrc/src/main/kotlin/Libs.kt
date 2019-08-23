0985c5814d67077230a79fc53bc5fa778e56a63c
Maxi Rosson <maxirosson@gmail.com>
2019-08-22 23:23:36 -0300

    Migrated dependencies to buildSrc

---
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -0,0 +1,97 @@
+object Libs {
+    const val google_material = "com.google.android.material:material:1.0.0"
+
+    const val play_core = "com.google.android.play:core:1.6.1"
+
+    // https://firebase.google.com/support/release-notes/android
+    const val firebase_core = "com.google.firebase:firebase-core:17.0.1"
+    const val firebase_appindexing = "com.google.firebase:firebase-appindexing:19.0.0"
+    const val firebase_perf = "com.google.firebase:firebase-perf:18.0.1"
+    const val firebase_dynamic_links = "com.google.firebase:firebase-dynamic-links:18.0.0"
+
+    const val firebase_client_android = "com.firebase:firebase-client-android:2.5.0"
+    const val firebase_dynamiclinks = "com.jdroidtools:jdroid-java-firebase-dynamiclinks:1.0.0"
+
+    // https://developer.android.com/topic/libraries/support-library/androidx-rn
+    const val androidx_annotation = "androidx.annotation:annotation:1.1.0"
+    const val androidx_appcompat = "androidx.appcompat:appcompat:1.0.2"
+    const val androidx_cardview = "androidx.cardview:cardview:1.0.0"
+    const val androidx_core = "androidx.core:core:1.0.2"
+    const val androidx_coordinatorlayout = "androidx.coordinatorlayout:coordinatorlayout:1.0.0"
+    const val androidx_drawerlayout = "androidx.drawerlayout:drawerlayout:1.0.0"
+    const val androidx_fragment = "androidx.fragment:fragment:1.0.0"
+    const val androidx_recyclerview = "androidx.recyclerview:recyclerview:1.0.0"
+    const val androidx_swiperefreshlayout = "androidx.swiperefreshlayout:swiperefreshlayout:1.0.0"
+    const val androidx_vectordrawable = "androidx.vectordrawable:vectordrawable:1.0.1"
+    const val androidx_lifecycle = "androidx.lifecycle:lifecycle-extensions:2.0.0"
+
+    // https://developer.android.com/jetpack/androidx/releases/room
+    const val androidx_room_runtime = "androidx.room:room-runtime:2.1.0"
+    const val androidx_room_compiler = "androidx.room:room-compiler:2.1.0"
+
+    // https://github.com/maxirosson/jdroid-java-http/blob/master/CHANGELOG.md
+    const val jdroid_java_http_core = "com.jdroidtools:jdroid-java-http-core:3.0.0-SNAPSHOT"
+    const val jdroid_java_http_okhttp = "com.jdroidtools:jdroid-java-http-okhttp:3.0.0-SNAPSHOT"
+
+    // https://github.com/maxirosson/jdroid-java/blob/master/CHANGELOG.md
+    const val jdroid_java_core = "com.jdroidtools:jdroid-java-core:3.0.0-SNAPSHOT"
+    const val jdroid_java_remote_config = "com.jdroidtools:jdroid-java-remote-config:3.0.0-SNAPSHOT"
+    const val jdroid_java_firebase_database = "com.jdroidtools:jdroid-java-firebase-database:3.0.0-SNAPSHOT"
+
+    const val firebase_ads = "com.google.firebase:firebase-ads:18.1.1"
+    const val firebase_auth = "com.google.firebase:firebase-auth:18.1.0"
+    const val firebase_config = "com.google.firebase:firebase-config:18.0.0"
+    const val firebase_database = "com.google.firebase:firebase-database:18.0.1"
+
+    // https://firebase.google.com/support/release-notes/android
+    const val firebase_messaging = "com.google.firebase:firebase-messaging:19.0.1"
+
+    // https://github.com/firebase/firebase-jobdispatcher-android/releases
+    const val firebase_jobdispatcher = "com.firebase:firebase-jobdispatcher:0.8.6"
+
+    const val crashlytics = "com.crashlytics.sdk.android:crashlytics:2.10.1"
+
+    // https://developers.google.com/android/guides/releases
+    const val play_services_base = "com.google.android.gms:play-services-base:17.0.0"
+    const val play_services_maps = "com.google.android.gms:play-services-maps:17.0.0"
+    const val play_services_games = "com.google.android.gms:play-services-games:18.0.0"
+    const val play_services_auth = "com.google.android.gms:play-services-auth:17.0.0"
+
+    // https://jcenter.bintray.com/com/android/billingclient/billing/
+    // https://developer.android.com/google/play/billing/billing_library_releases_notes
+    const val billing = "com.android.billingclient:billing:1.2.2"
+
+    // https://github.com/facebook/device-year-class/blob/master/CHANGELOG.md
+    const val facebook_yearclass = "com.facebook.device.yearclass:yearclass:2.1.0"
+
+    // https://github.com/square/leakcanary/blob/master/docs/changelog.md
+    const val leakcanary = "com.squareup.leakcanary:leakcanary-android:1.6.3"
+    const val leakcanary_no_op = "com.squareup.leakcanary:leakcanary-android-no-op:1.6.3"
+
+    // https://github.com/InsertKoinIO/koin/blob/master/CHANGELOG.md
+    const val koin = "org.koin:koin-android:2.0.1"
+    const val koin_viewmodel = "org.koin:koin-androidx-viewmodel:2.0.1"
+
+    // https://github.com/twitter/twitter-kit-android/wiki/Changelog
+    const val twitter = "com.twitter.sdk.android:tweet-ui:3.3.0"
+
+    const val slf4j_android = "org.slf4j:slf4j-android:1.6.1-RC1"
+
+    // https://github.com/bumptech/glide/releases
+    const val glide = "com.github.bumptech.glide:glide:4.9.0"
+    const val glide_compiler = "com.github.bumptech.glide:compiler:4.9.0"
+    const val glide_okhttp3 = "com.github.bumptech.glide:okhttp3-integration:4.9.0"
+
+    // https://developers.facebook.com/docs/android
+    const val facebook_core = "com.facebook.android:facebook-core:4.41.0"
+    const val facebook_login = "com.facebook.android:facebook-login:4.41.0"
+
+    const val junit = "junit:junit:4.12"
+
+    // https://github.com/robolectric/robolectric
+    const val robolectric = "org.robolectric:robolectric:4.3"
+
+    const val espresso_core = "androidx.test.espresso:espresso-core:3.2.0"
+    const val espresso_contrib = "androidx.test.espresso:espresso-contrib:3.1.1"
+    const val test_runner = "androidx.test:runner:1.2.0"
+}
