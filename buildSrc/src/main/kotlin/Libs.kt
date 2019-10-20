5950a65b8c0f8e8ac5bc6e2a5ef657bcaf821e9a
jdroid-ci <jdroidtools@gmail.com>
2019-10-20 00:01:53 +0000

    Upgraded com.google.firebase:firebase-database from 19.1.0 to 19.2.0

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -9,8 +9,8 @@ object Libs {
     const val firebase_auth = "com.google.firebase:firebase-auth:19.1.0"
     const val firebase_appindexing = "com.google.firebase:firebase-appindexing:19.0.0"
     const val firebase_client_android = "com.firebase:firebase-client-android:2.5.2"
-    const val firebase_config = "com.google.firebase:firebase-config:19.0.3"
-    const val firebase_database = "com.google.firebase:firebase-database:19.1.0"
+    const val firebase_config = "com.google.firebase:firebase-config:19.0.2"
+    const val firebase_database = "com.google.firebase:firebase-database:19.2.0"
     const val firebase_dynamic_links = "com.google.firebase:firebase-dynamic-links:19.0.0"
     const val firebase_perf = "com.google.firebase:firebase-perf:19.0.0"
 
