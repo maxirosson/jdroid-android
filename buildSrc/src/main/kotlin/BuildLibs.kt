cf58781a28f045985f277322fa8e14c7e6f15d76
Maxi Rosson <maxirosson@gmail.com>
2020-01-09 22:41:54 -0300

    Upgrade component builder version

--- buildSrc/src/main/kotlin/BuildLibs.kt
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -1,6 +1,6 @@
 object BuildLibs {
     // https://github.com/maxirosson/jdroid-component-builder/blob/master/CHANGELOG.md
-    const val jdroid_component_builder_plugin = "com.jdroidtools:jdroid-component-builder:2.2.0"
+    const val jdroid_component_builder_plugin = "com.jdroidtools:jdroid-component-builder:2.3.0-SNAPSHOT"
 
     // https://github.com/maxirosson/jdroid-gradle-plugin/releases
     const val jdroid_gradle_android_plugin = "com.jdroidtools:jdroid-gradle-android-plugin:2.4.0-SNAPSHOT"
