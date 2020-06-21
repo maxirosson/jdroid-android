ac0f209daaec9bd5cab6a795c26e2bf3efcb3413
jdroid-ci <jdroidtools@gmail.com>
2020-06-21 00:08:56 +0000

    Upgraded com.google.firebase:firebase-ads from 19.1.0 to 19.2.0

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -4,7 +4,7 @@ object Libs {
     const val play_core = "com.google.android.play:core:1.7.3"
 
     // https://firebase.google.com/support/release-notes/android
-    const val firebase_ads = "com.google.firebase:firebase-ads:19.1.0"
+    const val firebase_ads = "com.google.firebase:firebase-ads:19.2.0"
     const val firebase_analytics = "com.google.firebase:firebase-analytics:17.4.3"
     const val firebase_auth = "com.google.firebase:firebase-auth:19.3.1"
     const val firebase_appindexing = "com.google.firebase:firebase-appindexing:19.1.0"
