6f801cf4d9e527eda247bae27471fa6f495df6eb
Jdroid CI <jdroidtools@gmail.com>
2020-09-20 23:24:17 -0300

    Upgraded dependencies for groupId com.google.firebase (#503)
    * Upgraded com.google.firebase:firebase-ads from 19.1.0 to 19.2.0
    
    * Upgraded com.google.firebase:firebase-crashlytics from 17.0.1 to 17.1.0
    
    * Upgraded com.google.firebase:firebase-messaging from 20.2.0 to 20.2.1
    
    * Upgraded com.google.firebase:firebase-database from 19.3.0 to 19.3.1
    
    * Upgraded com.google.firebase:firebase-analytics from 17.4.3 to 17.4.4
    
    * Upgraded com.google.firebase:firebase-auth from 19.3.1 to 19.3.2
    
    * Upgraded com.google.firebase:firebase-crashlytics from 17.1.0 to 17.1.1
    
    * Upgraded com.google.firebase:firebase-config from 19.1.4 to 19.2.0
    
    * Upgraded com.google.firebase:firebase-messaging from 20.2.1 to 20.2.3
    
    * Upgraded com.google.firebase:firebase-perf from 19.0.7 to 19.0.8
    
    * Upgraded com.google.firebase:firebase-messaging from 20.2.3 to 20.2.4
    
    * Upgraded com.google.firebase:firebase-analytics from 17.4.4 to 17.5.0
    
    * Upgraded com.google.firebase:firebase-crashlytics from 17.1.1 to 17.2.1
    
    * Upgraded com.google.firebase:firebase-database from 19.3.1 to 19.4.0
    
    * Upgraded com.google.firebase:firebase-ads from 19.2.0 to 19.4.0
    
    * Upgraded com.google.firebase:firebase-auth from 19.3.2 to 19.4.0
    
    Co-authored-by: Maxi Rosson <maxirosson@gmail.com>

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -4,17 +4,17 @@ object Libs {
     const val play_core = "com.google.android.play:core:1.7.3"
 
     // https://firebase.google.com/support/release-notes/android
-    const val firebase_ads = "com.google.firebase:firebase-ads:19.1.0"
-    const val firebase_analytics = "com.google.firebase:firebase-analytics:17.4.3"
-    const val firebase_auth = "com.google.firebase:firebase-auth:19.3.1"
+    const val firebase_ads = "com.google.firebase:firebase-ads:19.4.0"
+    const val firebase_analytics = "com.google.firebase:firebase-analytics:17.5.0"
+    const val firebase_auth = "com.google.firebase:firebase-auth:19.4.0"
     const val firebase_appindexing = "com.google.firebase:firebase-appindexing:19.1.0"
     const val firebase_client_android = "com.firebase:firebase-client-android:2.5.2"
-    const val firebase_config = "com.google.firebase:firebase-config:19.1.4"
-    const val firebase_database = "com.google.firebase:firebase-database:19.3.0"
+    const val firebase_config = "com.google.firebase:firebase-config:19.2.0"
+    const val firebase_database = "com.google.firebase:firebase-database:19.4.0"
     const val firebase_dynamic_links = "com.google.firebase:firebase-dynamic-links:19.1.0"
-    const val firebase_perf = "com.google.firebase:firebase-perf:19.0.7"
-    const val firebase_messaging = "com.google.firebase:firebase-messaging:20.2.0"
-    const val firebase_crashlytics = "com.google.firebase:firebase-crashlytics:17.0.1"
+    const val firebase_perf = "com.google.firebase:firebase-perf:19.0.8"
+    const val firebase_messaging = "com.google.firebase:firebase-messaging:20.2.4"
+    const val firebase_crashlytics = "com.google.firebase:firebase-crashlytics:17.2.1"
 
     // https://developer.android.com/topic/libraries/support-library/androidx-rn
     const val androidx_annotation = "androidx.annotation:annotation:1.1.0"
