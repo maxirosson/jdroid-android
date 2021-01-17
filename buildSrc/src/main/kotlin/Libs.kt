416b3f2473f6e7f4055208d949073b374eb2b86e
jdroid-ci <jdroidtools@gmail.com>
2021-01-17 00:09:19 +0000

    Upgraded com.google.firebase:firebase-analytics from 18.0.0 to 18.0.1

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -5,7 +5,7 @@ object Libs {
 
     // https://firebase.google.com/support/release-notes/android
     const val firebase_ads = "com.google.firebase:firebase-ads:19.6.0"
-    const val firebase_analytics = "com.google.firebase:firebase-analytics:18.0.0"
+    const val firebase_analytics = "com.google.firebase:firebase-analytics:18.0.1"
     const val firebase_auth = "com.google.firebase:firebase-auth:20.0.1"
     const val firebase_appindexing = "com.google.firebase:firebase-appindexing:19.1.0"
     const val firebase_client_android = "com.firebase:firebase-client-android:2.5.2"
